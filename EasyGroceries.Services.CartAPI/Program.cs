984a3f5556f203e3c43b799333a4dacbdf01f59e
Bhushan91086 <bhushanarvekar@gmail.com>
2023-10-13 11:40:11 +0530

    Added custom exception filter

--- EasyGroceries.Services.CartAPI/Program.cs
+++ EasyGroceries.Services.CartAPI/Program.cs
@@ -1,4 +1,5 @@
 using EasyGroceries.Cart.Application;
+using EasyGroceries.Cart.Application.Filters;
 using EasyGroceries.Cart.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureApplicationServices(builder.Configuration);
 builder.Services.ConfigureInfrastructureServices();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(x => {
+    x.Filters.Add<CustomExceptionFilter>();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
